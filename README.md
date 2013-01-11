82d5a35dae7dc7eda666733c6974f43bf175c584
Mugunth <mugunth@gmail.com>
2013-01-11 17:40:40 +1000

    uplaoded some basic files for cd - it include non free autorun.exe which needs to be replaced later with a free alternative

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ thamizha-cd
 ===========
 
 Repository for thamizha cd releases
+
+This project aims at providing various useful free/opensource softwares in a CD format.
---
+++ download.bat
@@ -0,0 +1,13 @@
+REM download ekalappai version: 3.1-dev
+wget "http://ekalappai.googlecode.com/files/eKalappai-3.1-dev-installer.exe" 
+
+REM download firefox tamil version
+REM actual ftp location = http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/18.0/win32/ta/Firefox%20Setup%20Stub%2018.0.exe 
+REM but its advisable to use the url from mozilla download page.
+wget "http://download.mozilla.org/?lang=ta&product=firefox-18.0&os=win" -O firefox-18.0-tamil-installer.exe
+
+REM download money manager version: 0.9.9.0
+wget "http://www.codelathe.com/mmex/installer/windows/mmex_0.9.9.0_win32_setup.exe"
+
+REM download money manager version: 0.9.9.0
+wget "http://www.codelathe.com/mmex/installer/windows/mmex_0.9.9.0_win32_setup.exe"
