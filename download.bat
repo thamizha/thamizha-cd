f540bb03c875285e72e03f87ac4cf770810e904a
Mugunth <mugunth@gmail.com>
2013-01-12 17:45:44 +1000

    updated files as of today

--- download.bat
+++ download.bat
@@ -8,6 +8,3 @@ wget "http://download.mozilla.org/?lang=ta&product=firefox-18.0&os=win" -O firef
 
 REM download money manager version: 0.9.9.0
 wget "http://www.codelathe.com/mmex/installer/windows/mmex_0.9.9.0_win32_setup.exe"
-
-REM download money manager version: 0.9.9.0
-wget "http://www.codelathe.com/mmex/installer/windows/mmex_0.9.9.0_win32_setup.exe"
